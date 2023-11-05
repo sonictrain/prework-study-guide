f77e32e0ea3399156505b9d831e9519063547865
sonictrain <nicola.brucoli92@gmail.com>
2023-11-05 17:51:42 +0000

    Added JS to the project

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x ++) {
+    if (topics[x] === 'HTML') {
+        console.log("Let's study HTML!");
+        } else if (topics[x] === 'CSS') {
+        console.log("Let's study CSS!");
+        } else if (topics[x] === 'Git') {
+        console.log("Let's study Git!");
+        } else if (topics[x] === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+        } else {
+        console.log('Please try again!');
+    }  
+}
